fe8f45b3fc5d1b5ffb7f35f2d41a9e0bc24d3936
giovani.franz <giovani.franz@agi.com.br>
2023-01-06 11:34:18 -0300

    feat/add export default home page

--- src/containers/home/index.tsx
+++ src/containers/home/index.tsx
@@ -1,15 +1,32 @@
 import { useEffect, useState } from 'react';
+import { dehydrate, QueryClient } from '@tanstack/react-query';
+import type { GetStaticProps } from 'next';
 import Head from 'next/head';
 
 import { Layout } from '@/components/Layout';
 import { useAnimeRandom } from '@/hooks/useAnime';
+import { prefetchAnimeRandom, prefetchQuoteRandom } from '@/services/http';
 import { useSearch } from '@/stores/useSearch';
 import type { AnimeResult } from '@/types/results';
 
 import { AnimeBanner, Quote, Search } from './components';
 import { Box, Container, Heading } from './styles';
 
-export function Home() {
+export const getStaticProps: GetStaticProps = async () => {
+  const queryClient = new QueryClient();
+
+  await prefetchAnimeRandom(queryClient);
+  await prefetchQuoteRandom(queryClient);
+
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
+  };
+};
+
+export default function Home() {
   const [animesByTitle, setAnimeByTitle] = useState<AnimeResult[]>([]);
   const { data: animeRandom } = useAnimeRandom();
   const results = useSearch((state) => state.results);
