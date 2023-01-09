62aae09f90614431c7a2baf12d7bb73c41a4c5a7
giovani.franz <giovani.franz@agi.com.br>
2023-01-09 16:56:14 -0300

    feat/implements turborepo/remove packages

--- design-system/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./Avatar";
-export * from "./Button";
-export * from "./Header";
-export * from "./Text";
-export * from "./stitches";
--- src/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './colors';
-export * from './fonts';
-export * from './fontSizes';
-export * from './fontWeights';
-export * from './lineHeights';
-export * from './radii';
-export * from './space';
